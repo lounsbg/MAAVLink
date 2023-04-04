961dd16236dbb6d5e5d6f3d5a5705ceed6e1d862
Gabriel Lounsbury <lounsbg@umich.edu>
2023-04-03 20:34:07 -0400

    Initial commit: converted arduino code to stm

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -47,8 +47,8 @@ mavlink_status_t status;
 mavlink_message_t msg;
 int chan  = MAVLINK_COMM_0; //use channel 0 only if 1 communication channel used
 mavlink_system_t mavlink_system = {
-		systemID,
-		componentID
+systemID,
+componentID
 };
 /* USER CODE END PV */
 
@@ -70,8 +70,8 @@ void SystemClock_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	//define system type
-	uint8_t system_type = MAV_TYPE_GENERIC;
+//define system type
+uint8_t system_type = MAV_TYPE_GENERIC;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -141,7 +141,139 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+PUTCHAR_PROTOTYPE
+{
+  HAL_UART_Transmit(&hlpuart1, (uint8_t *)&ch, 1, 0xFFFF);
+  return ch;
+}
 
+void Mav_Request_Data() {
+mavlink_message_t msg;
+uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+
+// STREAMS that can be requested
+ /*
+  * Definitions are in common.h: enum MAV_DATA_STREAM
+  *
+  * MAV_DATA_STREAM_ALL=0, // Enable all data streams
+  * MAV_DATA_STREAM_RAW_SENSORS=1, /* Enable IMU_RAW, GPS_RAW, GPS_STATUS packets.
+  * MAV_DATA_STREAM_EXTENDED_STATUS=2, /* Enable GPS_STATUS, CONTROL_STATUS, AUX_STATUS
+  * MAV_DATA_STREAM_RC_CHANNELS=3, /* Enable RC_CHANNELS_SCALED, RC_CHANNELS_RAW, SERVO_OUTPUT_RAW
+  * MAV_DATA_STREAM_RAW_CONTROLLER=4, /* Enable ATTITUDE_CONTROLLER_OUTPUT, POSITION_CONTROLLER_OUTPUT, NAV_CONTROLLER_OUTPUT.
+  * MAV_DATA_STREAM_POSITION=6, /* Enable LOCAL_POSITION, GLOBAL_POSITION/GLOBAL_POSITION_INT messages.
+  * MAV_DATA_STREAM_EXTRA1=10, /* Dependent on the autopilot
+  * MAV_DATA_STREAM_EXTRA2=11, /* Dependent on the autopilot
+  * MAV_DATA_STREAM_EXTRA3=12, /* Dependent on the autopilot
+  * MAV_DATA_STREAM_ENUM_END=13,
+  *
+  * Data in PixHawk available in:
+  *  - Battery, amperage and voltage (SYS_STATUS) in MAV_DATA_STREAM_EXTENDED_STATUS
+  *  - Gyro info (IMU_SCALED) in MAV_DATA_STREAM_EXTRA1
+  */
+
+ // To be setup according to the needed information to be requested from the Pixhawk
+ const int  maxStreams = 2;
+ const uint8_t MAVStreams[maxStreams] = {MAV_DATA_STREAM_EXTENDED_STATUS, MAV_DATA_STREAM_EXTRA1};
+ const uint16_t MAVRates[maxStreams] = {0x02,0x05};
+
+ for (int i=0; i < maxStreams; i++) {
+   /*
+    * mavlink_msg_request_data_stream_pack(system_id, component_id,
+    *    &msg,
+    *    target_system, target_component,
+    *    MAV_DATA_STREAM_POSITION, 10000000, 1);
+    *
+    * mavlink_msg_request_data_stream_pack(uint8_t system_id, uint8_t component_id,
+    *    mavlink_message_t* msg,
+    *    uint8_t target_system, uint8_t target_component, uint8_t req_stream_id,
+    *    uint16_t req_message_rate, uint8_t start_stop)
+    *
+    */
+   mavlink_msg_request_data_stream_pack(2, 200, &msg, 1, 0, MAVStreams[i], MAVRates[i], 1);
+   uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+   //write output to UART
+   HAL_UART_Transmit(&huart2, (uint16_t *)(&msg), sizeof(msg), HAL_MAX_DELAY);
+ }
+}
+void comm_receive() {
+  mavlink_message_t msg;
+  mavlink_status_t status;
+
+
+  while(HAL_UART_Receive(&huart2, (uint16_t *)(&status), 1, 0) == HAL_OK) {
+    uint8_t c = HAL_UART_Receive(&huart2, (uint16_t *)(&msg), sizeof(msg), HAL_MAX_DELAY);
+
+    // Try to get a new message
+    if(mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status)) {
+
+      // Handle message
+      switch(msg.msgid) {
+        case MAVLINK_MSG_ID_HEARTBEAT:  // #0: Heartbeat
+          {
+            // E.g. read GCS heartbeat and go into
+            // comm lost mode if timer times out
+        printf("Pixhawk 4 Heartbeat Received! \n\r");
+          }
+          break;
+
+        case MAVLINK_MSG_ID_SYS_STATUS:  // #1: SYS_STATUS
+          {
+            /* Message decoding: PRIMITIVE
+             *    mavlink_msg_sys_status_decode(const mavlink_message_t* msg, mavlink_sys_status_t* sys_status)
+             */
+            //mavlink_message_t* msg;
+            mavlink_sys_status_t sys_status;
+            mavlink_msg_sys_status_decode(&msg, &sys_status);
+          }
+          break;
+
+        case MAVLINK_MSG_ID_PARAM_VALUE:  // #22: PARAM_VALUE
+          {
+            /* Message decoding: PRIMITIVE
+             *    mavlink_msg_param_value_decode(const mavlink_message_t* msg, mavlink_param_value_t* param_value)
+             */
+            //mavlink_message_t* msg;
+            mavlink_param_value_t param_value;
+            mavlink_msg_param_value_decode(&msg, &param_value);
+          }
+          break;
+
+        case MAVLINK_MSG_ID_RAW_IMU:  // #27: RAW_IMU
+          {
+            /* Message decoding: PRIMITIVE
+             *    static inline void mavlink_msg_raw_imu_decode(const mavlink_message_t* msg, mavlink_raw_imu_t* raw_imu)
+             */
+            mavlink_raw_imu_t raw_imu;
+            mavlink_msg_raw_imu_decode(&msg, &raw_imu);
+          }
+          break;
+
+        case MAVLINK_MSG_ID_ATTITUDE:  // #30
+          {
+            /* Message decoding: PRIMITIVE
+             *    mavlink_msg_attitude_decode(const mavlink_message_t* msg, mavlink_attitude_t* attitude)
+             */
+            mavlink_attitude_t attitude;
+            mavlink_msg_attitude_decode(&msg, &attitude);
+
+            if(attitude.roll>1) leds_modo = 0;
+            else if(attitude.roll<-1) leds_modo = 2;
+            else leds_modo=1;
+          }
+          break;
+
+
+       default:
+          break;
+      }
+    }
+  }
+}
 /* USER CODE END 4 */
 
 /**
@@ -175,3 +307,4 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
+
